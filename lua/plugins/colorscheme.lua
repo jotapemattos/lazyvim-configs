d26f1075823746d11ebc1796ae70270973b534eb
João Pedro <jpmrc49@gmail.com>
2024-03-19 16:36:05 -0300

    feat: adds min-theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -18,7 +18,25 @@ return {
       })
     end,
   },
-
+  {
+    "datsfilipe/min-theme.nvim",
+    lazy = true,
+    config = function()
+      require("min-theme").setup({
+        -- (note: if your configuration sets vim.o.background the following option will do nothing!)
+        theme = "dark", -- String: 'dark' or 'light', determines the colorscheme used
+        transparent = true, -- Boolean: Sets the background to transparent
+        italics = {
+          comments = true, -- Boolean: Italicizes comments
+          keywords = true, -- Boolean: Italicizes keywords
+          functions = true, -- Boolean: Italicizes functions
+          strings = true, -- Boolean: Italicizes strings
+          variables = true, -- Boolean: Italicizes variables
+        },
+        overrides = {}, -- A dictionary of group names, can be a function returning a dictionary or a table.
+      })
+    end,
+  },
   { "rose-pine/neovim", name = "rose-pine" },
 
   {
@@ -63,7 +81,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "vesper",
+      colorscheme = "min-theme",
     },
   },
 }
